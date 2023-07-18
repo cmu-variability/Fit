ae26cf1261361bac9d969d07f06af3c6c11bddc3
ala <ala@aladembp.mynetworksettings.com>
2023-07-18 18:33:54 -0400

    adjust mic/camera

--- public/script.js
+++ public/script.js
@@ -9,11 +9,13 @@ const myPeer = new Peer(undefined, {
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
+let mediaStream;
 
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
+  mediaStream=stream
   addVideoStream(myVideo, stream)
 
   myPeer.on('call', call => {
@@ -79,5 +81,164 @@ leaveCallButton.addEventListener('click', () => {
     window.location.href = '/';
 });
 
+const toggleMic = document.getElementById('toggle-mic');
+const toggleCamera = document.getElementById('toggle-camera');
+
+function toggleMicrophone() {
+  if (mediaStream) {
+    const audioTracks = mediaStream.getAudioTracks();
+    audioTracks.forEach(track => {
+      track.enabled = !track.enabled;
+    });
+  }
+}
+
+// Function to toggle the camera track
+function toggleCameraStream() {
+  if (mediaStream) {
+    const videoTracks = mediaStream.getVideoTracks();
+    videoTracks.forEach(track => {
+      track.enabled = !track.enabled;
+    });
+  }
+}
+
+// Event listener for microphone toggle
+toggleMic.addEventListener('click', () => {
+  toggleMicrophone();
+});
+
+// Event listener for camera toggle
+toggleCamera.addEventListener('click', () => {
+  toggleCameraStream();
+});
 
 
+
+
+// const toggleMicButton = document.getElementById('toggle-mic');
+// let isMicOn = true;
+
+// toggleMicButton.addEventListener('click', () => {
+//   if (mediaStream) {
+//     const audioTrack = mediaStream.getAudioTracks()[0];
+//     isMicOn = !isMicOn;
+
+//     // Enable or disable the audio track based on the microphone state
+//     audioTrack.enabled = isMicOn;
+
+//     // Update the UI based on the microphone state
+//     toggleMicButton.innerText = isMicOn ? 'Mute Microphone' : 'Unmute Microphone';
+//   }
+// });
+
+// const toggleCameraButton = document.getElementById('toggle-camera');
+// let isCameraOn = true;
+
+// toggleCameraButton.addEventListener('click', () => {
+//   if (mediaStream) {
+//     const videoTrack = mediaStream.getVideoTracks()[0];
+//     isCameraOn = !isCameraOn;
+
+//     // Enable or disable the video track based on the camera state
+//     videoTrack.enabled = isCameraOn;
+
+//     // Update the UI based on the camera state
+//     toggleCameraButton.innerText = isCameraOn ? 'Turn Off Camera' : 'Turn On Camera';
+//   }
+// });
+
+
+let selectedAudioDeviceId;
+let selectedVideoDeviceId;
+
+// Get references to the audio and video device dropdowns
+const audioDeviceDropdown = document.getElementById('audio-device-dropdown');
+const videoDeviceDropdown = document.getElementById('video-device-dropdown');
+
+// Function to populate the dropdown options
+function populateDeviceOptions(deviceList, dropdown) {
+  // Clear existing options
+  dropdown.innerHTML = '';
+
+  // Create a default option
+  const defaultOption = document.createElement('option');
+  defaultOption.value = '';
+  defaultOption.disabled = true;
+  defaultOption.selected = true;
+  defaultOption.textContent = 'Choose Device';
+  dropdown.appendChild(defaultOption);
+
+  // Create an option for each device in the list
+  deviceList.forEach(device => {
+    const option = document.createElement('option');
+    option.value = device.deviceId;
+    option.textContent = device.label || `Device ${device.deviceId}`;
+    dropdown.appendChild(option);
+  });
+}
+
+// Function to handle the updated media stream
+function handleUpdatedMediaStream(stream) {
+  // Update the media stream in your WebRTC connection or media elements
+  // For example, if you have a video element with id 'localVideo':
+  const videoElement = document.getElementById('localVideo');
+  if (videoElement) {
+    // Stop the existing media stream if it is currently playing
+    if (videoElement.srcObject) {
+      const tracks = videoElement.srcObject.getTracks();
+      tracks.forEach(track => track.stop());
+    }
+    // Assign the new media stream to the video element
+    videoElement.srcObject = stream;
+  }
+}
+
+// Function to update the media stream with the selected devices
+function updateMediaStream() {
+  const constraints = {
+    audio: selectedAudioDeviceId ? { deviceId: selectedAudioDeviceId } : isMicOn,
+    video: selectedVideoDeviceId ? { deviceId: selectedVideoDeviceId } : isCameraOn
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(stream => {
+      // Store the new media stream for later use
+      mediaStream = stream;
+
+      // Call the function to handle the updated media stream
+      handleUpdatedMediaStream(stream);
+    })
+    .catch(error => {
+      console.error('Error accessing media devices:', error);
+    });
+}
+
+// Event listener for audio device selection
+audioDeviceDropdown.addEventListener('change', () => {
+  selectedAudioDeviceId = audioDeviceDropdown.value;
+  updateMediaStream();
+});
+
+// Event listener for video device selection
+videoDeviceDropdown.addEventListener('change', () => {
+  selectedVideoDeviceId = videoDeviceDropdown.value;
+  updateMediaStream();
+});
+
+// Retrieve the list of available audio and video devices
+navigator.mediaDevices.enumerateDevices()
+  .then(devices => {
+    const audioDevices = devices.filter(device => device.kind === 'audioinput');
+    const videoDevices = devices.filter(device => device.kind === 'videoinput');
+
+    // Populate the audio and video device dropdowns
+    populateDeviceOptions(audioDevices, audioDeviceDropdown);
+    populateDeviceOptions(videoDevices, videoDeviceDropdown);
+
+    // Initially update the media stream with the selected devices
+    updateMediaStream();
+  })
+  .catch(error => {
+    console.error('Error enumerating devices:', error);
+  });
