cafe3bb44cf4583b733e1a3eb74384d0f652cd27
Aditya Ganesh <adityawolverines@gmail.com>
2023-10-07 00:49:23 -0400

    fixed conflict with Adam and I; removed render critical moments code --> will implement that myself since I pretty much have that code done already

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,157 +1,172 @@
-
 // Firebase setup
 const firebaseConfig = {
-    apiKey: "AIzaSyD9blp9GQZ50XqLM8VCnH7pjxt68JT7FBQ",
-    authDomain: "fit-project-88b55.firebaseapp.com",
-    databaseURL: "https://fit-project-88b55-default-rtdb.firebaseio.com",
-    projectId: "fit-project-88b55",
-    storageBucket: "fit-project-88b55.appspot.com",
-    messagingSenderId: "569706388901",
-    appId: "1:569706388901:web:1e47d6bdd5c395c5bd9c4c"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
+  apiKey: "AIzaSyD9blp9GQZ50XqLM8VCnH7pjxt68JT7FBQ",
+  authDomain: "fit-project-88b55.firebaseapp.com",
+  databaseURL: "https://fit-project-88b55-default-rtdb.firebaseio.com",
+  projectId: "fit-project-88b55",
+  storageBucket: "fit-project-88b55.appspot.com",
+  messagingSenderId: "569706388901",
+  appId: "1:569706388901:web:1e47d6bdd5c395c5bd9c4c"
+};
 
-    // Reference to the location where data is stored (can change this if data should be separated)
-    const criticalMomentsRef = database.ref('criticalMoments');
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
-  
-  // Find the "Mark Critical Moment" button element and Notes buttons
-  const markCriticalButton = document.getElementById('mainCriticalMomentButton');
-  const markCriticalOptionButton = document.getElementById('criticalMomentOptionButton');
-  const notesPopup = document.getElementById('notesPopup');
-  const notesInput = document.getElementById('notesInput');
-  const continueButton = document.getElementById('continueButton');
-  
-  // Initialize the data object
-  let dataToStore;
-  
-  
-  // Add click event listener to the button
-  markCriticalButton.addEventListener('click', () => {
-    // Extract shared data
-    const sessionID = ROOM_ID;
-    const timestamp = new Date().getTime();
-    const formattedTimestamp = new Date(timestamp).toLocaleString();
-  
-    if (userRole === "Researcher") {
-      // Show the notes popup
-      notesPopup.style.display = 'block';
-  
-      // Add event listener for the Continue button when notes are submitted
-      continueButton.addEventListener('click', () => {
-        const notes = notesInput.value;
-  
-        // Create an object with the data to be stored (with notes)
-        dataToStore = {
-          username: userName,
-          role: userRole,
-          sessionid: sessionID,
-          timestamp: formattedTimestamp,
-          notes: notes
-        };
-  
-        // Hide the popup after storing the data
-        notesPopup.style.display = 'none';
-          
-        // Check if the reference exists
-        criticalMomentsRef.once('value', snapshot => {
-          if (!snapshot.exists()) {
-            // The reference doesn't exist, so you can create it and set initial data as empty
-            criticalMomentsRef.set({});
-          }
-        });
-  
-        // Push the data to the Firebase database 
-        // Needs to occur separately from else statement due to asynchronous execution
-        criticalMomentsRef.push(dataToStore)
-          .then(() => {
-            console.log('Data stored in Firebase!');
-          })
-          .catch(error => {
-            console.error('Error storing data:', error);
-          });
-      });
-    } 
-    else {
-      // Create an object with the data to be stored (without notes)
+// Find the "Mark Critical Moment" button element and Notes buttons
+const markCriticalButton = document.getElementById('mainCriticalMomentButton');
+const markCriticalOptionButton = document.getElementById('criticalMomentOptionButton');
+const notesPopup = document.getElementById('notesPopup');
+const notesInput = document.getElementById('notesInput');
+const continueButton = document.getElementById('continueButton');
+
+// Initialize the data object
+let dataToStore;
+
+// Define the updateTableWithNewData function
+function updateTableWithNewData(newData) {
+  const table = document.querySelector('.data-table');
+  // Create a new table row
+  const newRow = table.insertRow();
+
+  // Create cells for the table row
+  const timeCell = newRow.insertCell(0);
+  const researcherCell = newRow.insertCell(1);
+  const commentCell = newRow.insertCell(2);
+
+  // Populate the cells with the new data
+  timeCell.textContent = newData.timestamp;
+  researcherCell.textContent = newData.username;
+  commentCell.textContent = newData.notes;   
+}
+
+// Add click event listener to the button
+markCriticalButton.addEventListener('click', () => {
+  // Extract shared data
+  const sessionID = ROOM_ID;
+  const timestamp = new Date().getTime();
+  const formattedTimestamp = new Date(timestamp).toLocaleString();
+
+  if (userRole.toLowerCase() === "researcher") {
+    // Show the notes popup
+    notesPopup.style.display = 'block';
+
+    // Add event listener for the Continue button when notes are submitted
+    continueButton.addEventListener('click', () => {
+      const notes = notesInput.value;
+
+      // Create an object with the data to be stored (with notes)
       dataToStore = {
         username: userName,
         role: userRole,
-        timestamp: formattedTimestamp
+        sessionid: sessionID,
+        timestamp: formattedTimestamp,
+        notes: notes
       };
-  
-      // Use sessionID to create a reference within criticalMoments
-      const sessionRef = database.ref(`criticalMoments/${sessionID}`);
-  
-      // Push the data to the session-specific reference in Firebase
-      sessionRef.push(dataToStore)
+
+      // Hide the popup after storing the data
+      notesPopup.style.display = 'none';
+        
+      // Reference to the location where data is stored (can change this if data should be separated)
+      const criticalMomentsRef = database.ref('criticalMoments').child(sessionID);
+
+      // Listen for changes to the specified session's data
+      criticalMomentsRef.on('child_added', snapshot => {
+        const newData = snapshot.val();
+        updateTableWithNewData(newData);
+      });
+
+      // Check if the reference exists
+      criticalMomentsRef.once('value', snapshot => {
+        if (!snapshot.exists()) {
+          // The reference doesn't exist, so you can create it and set initial data as empty
+          criticalMomentsRef.set({});
+        }
+      });
+
+      // Push the data to the Firebase database 
+      // Needs to occur separately from else statement due to asynchronous execution
+      criticalMomentsRef.push(dataToStore)
         .then(() => {
-          console.log('Data stored in Firebase!!');
+          console.log('Data stored in Firebase!');
         })
         .catch(error => {
           console.error('Error storing data:', error);
         });
-    }
-  
-  });
-  
-  
-  //when first half of the meeting end:
-  //participant click leave call button to go to the second phase
-  //researcher click leave call button to see a list of participant that are in the second phase
-  const leaveCallButton = document.getElementById('leaveCallButton');
-  
-  leaveCallButton.addEventListener('click', () => {
-    
-    // Stop recording if userRole is null
-    if (userRole == null) {
-      recordRTC.stopRecording(function() {
-        let blob = recordRTC.getBlob();
-        invokeSaveAsDialog(blob);
-  
-        // Cleanup and then redirect
-        cleanupAndRedirect(`/${ROOM_ID}/waitingRoom`);
-      });
-    } else {
-      cleanupAndRedirect('/w');
-    }
-  
-  });
+    });
+  } 
+  else {
+    // Create an object with the data to be stored (without notes)
+    dataToStore = {
+      username: userName,
+      role: userRole,
+      sessionid: sessionID,
+      timestamp: formattedTimestamp
+    };
 
+    const criticalMomentsRef = database.ref('criticalMoments').child(sessionID);
 
-  function renderCriticalMoments() {
-      console.log("Rendering critical moments..."); 
-      const list = document.getElementById('critical-moments-list');
-      
-      // Just for testing: Clear the list and append a test message
-      list.innerHTML = 'Loading moments...'; 
-  
-      criticalMomentsRef.on('value', (snapshot) => {
-          const moments = snapshot.val();
-  
-          if (moments) {
-              list.innerHTML = ''; // Clear the test message
-              for (const session in moments) {
-                  for (const key in moments[session]) {
-                      const moment = moments[session][key];
-                      const listItem = document.createElement('li');
-                      listItem.textContent = `${moment.username} (${moment.role}) at ${moment.timestamp}`;
-                      list.appendChild(listItem);
-                  }
-              }
-          } else {
-              list.innerHTML = 'No moments found';
-          }
+    // Listen for changes to the specified session's data
+    criticalMomentsRef.on('child_added', snapshot => {
+      const newData = snapshot.val();
+      updateTableWithNewData(newData);
+    });
+
+    // Check if the reference exists
+    criticalMomentsRef.once('value', snapshot => {
+      if (!snapshot.exists()) {
+        // The reference doesn't exist, so you can create it and set initial data as empty
+        criticalMomentsRef.set({});
+      }
+    });
+
+    criticalMomentsRef.push(dataToStore)
+      .then(() => {
+        console.log('Data stored in Firebase!');
+      })
+      .catch(error => {
+        console.error('Error storing data:', error);
       });
+
   }
-  
-  document.addEventListener('DOMContentLoaded', (event) => {
-      console.log('DOM loaded!'); // Logging to console to verify if this event is triggered
-      renderCriticalMoments();
-  });
-  
+});
 
-// Call the function to render the moments
-renderCriticalMoments();
+// Use the global firebase object for authentication
+const auth = firebase.auth();
+
+// Function to sign in with Google
+function signInWithGoogle() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  // Add custom parameters to the Google provider
+  provider.setCustomParameters({
+    hd: 'andrew.cmu.edu', // Specify the allowed email domain
+  });
+
+  return auth.signInWithPopup(provider);
+}
+
+// Function to sign out
+function signOut() {
+  return firebase.auth().signOut();
+}
+
+// Listen for changes in the user's authentication state
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in.
+    console.log('User signed in:', user);
+  } else {
+    // No user is signed in.
+    console.log('User signed out');
+  }
+});
+
+// Function to handle Google Sign-In button click
+function handleSignInWithGoogleClick() {
+  signInWithGoogle().then(() => {
+    // User is signed in successfully, you can perform additional actions if needed
+    console.log('User signed in with Google successfully');
+  }).catch((error) => {
+    console.error('Error signing in with Google:', error);
+    alert('Error signing in with Google. Please try again.');
+  });
+}
